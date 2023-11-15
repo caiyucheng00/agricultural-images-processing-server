6a3b1fe73706c6b3aa95924c28cabf7a6a113143
Cyc <cyc000412@163.com>
2023-11-15 20:18:46 +0800

    test rice seedling

--- loop_check.py
+++ loop_check.py
@@ -32,49 +32,72 @@ def shifei(file_name):
 
 
 def spike(file_name):
+    source = "data_flask/flask_spike"
+    project = "static/result_spike"
+    Path(source).mkdir(parents=True, exist_ok=True)
+    Path(project).mkdir(parents=True, exist_ok=True)
+
     flag_name = file_name.split(os.sep)[-1].split(".")[0]
     update_dir("data_flask/flask_spike")  # 每次清空
     unzip(file_name, "data_flask", "flask_spike")
 
-    n = yolo_detect(weights=ROOT / 'spike.pt', source="data_flask/flask_spike", project="static/result_spike")
+    n = yolo_detect(weights=ROOT / 'spike.pt', source=source, project=project)
     number = []
     for tensor in n:
         number.append(tensor.item())
     txt_name = "static/result_spike/detect/" + flag_name + ".txt"
     with open(txt_name, 'w+') as file:
         for i in number:
-            file.write(str(i))
+            file.write(str(i) + '\n')
 
-    zip("static/result_spike/detect", r"D:\NETCIA\Data\10m\sample10", "result_" + flag_name)
+    dst_dir = r"D:\NETCIA\Data\10m\sample10"
+    zip("static/result_spike/detect", dst_dir, "result_" + flag_name)
 
 
 def rice(file_name):
-    base_path = os.path.dirname(__file__)  # 当前文件所在路径
-    update_dir()
-    source_image_path = file_name
-    destination_image_path = base_path + '/data_flask/images/' + file_name.split("/")[-1]
-    shutil.copy(source_image_path, destination_image_path)
+    source = "data_flask/flask_rice"
+    project = "static/result_rice"
+    Path(source).mkdir(parents=True, exist_ok=True)
+    Path(project).mkdir(parents=True, exist_ok=True)
 
-    n = yolo_detect(weights=ROOT / 'rice.pt')
-    number = n.item()
-    txt_name = "static/result/" + file_name.split("/")[-1].split(".")[0] + ".txt"
-    with open(txt_name, 'w+') as file:
-        file.write(str(number))
+    flag_name = file_name.split(os.sep)[-1].split(".")[0]
+    update_dir("data_flask/flask_rice")  # 每次清空
+    unzip(file_name, "data_flask", "flask_rice")
 
-
-def seedling(file_name):
-    base_path = os.path.dirname(__file__)  # 当前文件所在路径
-    update_dir()
-    source_image_path = file_name
-    destination_image_path = base_path + '/data_flask/images/' + file_name.split("/")[-1]
-    shutil.copy(source_image_path, destination_image_path)
-
-    n = yolo_detect(weights=ROOT / 'seedling.pt')
-    number = n.item()
-    txt_name = "static/result/" + file_name.split("/")[-1].split(".")[0] + ".txt"
+    n = yolo_detect(weights=ROOT / 'rice.pt', source=source, project=project)
+    number = []
+    for tensor in n:
+        number.append(tensor.item())
+    txt_name = "static/result_rice/detect/" + flag_name + ".txt"
     with open(txt_name, 'w+') as file:
         for i in number:
             file.write(str(i) + '\n')
+
+    dst_dir = r"D:\NETCIA\Data\10m\sample10"
+    zip("static/result_rice/detect", dst_dir, "result_" + flag_name)
+
+
+def seedling(file_name):
+    source = "data_flask/flask_seedling"
+    project = "static/result_seedling"
+    Path(source).mkdir(parents=True, exist_ok=True)
+    Path(project).mkdir(parents=True, exist_ok=True)
+
+    flag_name = file_name.split(os.sep)[-1].split(".")[0]
+    update_dir("data_flask/flask_seedling")  # 每次清空
+    unzip(file_name, "data_flask", "flask_seedling")
+
+    n = yolo_detect(weights=ROOT / 'seedling.pt', source=source, project=project)
+    number = []
+    for tensor in n:
+        number.append(tensor.item())
+    txt_name = "static/result_seedling/detect/" + flag_name + ".txt"
+    with open(txt_name, 'w+') as file:
+        for i in number:
+            file.write(str(i) + '\n')
+
+    dst_dir = r"D:\NETCIA\Data\10m\sample10"
+    zip("static/result_seedling/detect", dst_dir, "result_" + flag_name)
 
 
 def phe(file_name):
@@ -144,8 +167,8 @@ def check_for_changes(filename):
         else:
             print("文件没有新增行数")
 
-        time.sleep(1)
-        print("过了1s")
+        time.sleep(10)
+        print("过了10s")
 
 
 if __name__ == '__main__':
